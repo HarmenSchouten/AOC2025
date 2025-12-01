8514c6fe31bf551d5c140b8234d5f60e5df883a8
Harmen Schouten <harmenschouten@outlook.com>
2025-12-01 09:40:55 +0100

    Day 01

--- Day01/Part1.ts
+++ Day01/Part1.ts
@@ -1,3 +1,22 @@
 import "../utils/index.ts"
 
 const input = await Deno.readTextFile("Day01/input.txt");
+
+const answer = input
+    .splitLb()
+    .reduce((acc, state) => {
+        const isLeft = state.startsWith('L')
+        const number = state.ints()[0];
+
+        let newState = acc.curr + (isLeft ? -number : number)
+
+        while (newState > 99) newState -= 100
+        while (newState < 0) newState += 100
+
+        if (newState === 0) acc.zero += 1
+        acc.curr = newState
+
+        return acc;
+    }, { curr: 50, zero: 0 } as { curr: number, zero: number });
+
+console.log(answer.zero)
--- Day01/Part2.ts
+++ Day01/Part2.ts
@@ -1,3 +1,41 @@
 import "../utils/index.ts"
 
 const input = await Deno.readTextFile("Day01/input.txt");
+
+const lines = input
+    .splitLb()
+    .reduce((acc, state) => {
+        const isLeft = state.startsWith('L')
+        const number = state.ints()[0];
+
+        let zeros = 0
+        let remaining = number;
+        if (number >= 100)
+        {
+            zeros = Math.floor(number / 100)
+            remaining -= zeros * 100
+        }
+
+        let newState = acc.curr + (isLeft ? -remaining : remaining);
+
+        while (newState > 99)
+        {
+            newState -= 100;
+            if (newState != 0 && acc.curr !== 0) zeros++
+        }
+
+        while (newState < 0)
+        {
+            newState += 100
+            if (newState != 0 && acc.curr !== 0) zeros++
+        }
+
+        if (newState === 0) zeros++
+
+        return {
+            curr: newState,
+            zero: acc.zero + zeros
+        }
+    }, { curr: 50, zero: 0 } as { curr: number, zero: number });
+
+console.log(lines.zero)
