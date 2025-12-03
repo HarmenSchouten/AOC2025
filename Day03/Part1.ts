d8f513337c7ed5c7a7a8262570caf3ad8bb3d6ab
Harmen Schouten <harmenschouten@outlook.com>
2025-12-03 09:53:23 +0100

    Day 03

--- Day03/Part1.ts
+++ Day03/Part1.ts
@@ -6,13 +6,12 @@ const answer = input
     .map(x => x.match(/\d/g)?.map(Number) ?? [])
     .reduce((acc, state) => {
         let best = Number.MIN_SAFE_INTEGER;
-        for (let i = 0; i < state.length; i++)
-        {
-            const left = state[i]
-            for (let j = i + 1; j < state.length; j++)
-            {
-                const right = state[j]
-                if (best < (left * 10 + right)) best = (left * 10 + right)
+        for (let i = 0; i < state.length; i++) {
+            const first = state[i]
+            for (let j = i + 1; j < state.length; j++) {
+                const second = state[j]
+                const val = first * 10 + second
+                if (best < val) best = val
             }
         }
         return acc += best
--- Day03/Part2.ts
+++ Day03/Part2.ts
@@ -6,16 +6,12 @@ const answer = input
     .splitLb()
     .map(x => x.match(/\d/g)?.map(Number) ?? [])
     .reduce((acc, state) => {
-        
         // Trim state to target length by removing the last occurence of the 
         // smallest digit before any larger digit until the target length is reached
-        while (state.length > targetLength)
-        {
+        while (state.length > targetLength) {
             let minIdx = 0, minNum = 9;
-            for (let i = 0; i < state.length; i++)
-            {
-                if (state[i] <= minNum)
-                {
+            for (let i = 0; i < state.length; i++) {
+                if (state[i] <= minNum) {
                     minNum = state[i];
                     minIdx = i;
                 } else break
